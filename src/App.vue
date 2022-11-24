b16517d7d19291711e83c441c463ceff41932658
Mark <Mark@b.com>
2022-11-24 15:46:30 +0800

    移動功能

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,32 @@
 <template>
-  <div></div>
+  <div
+    id="canvas"
+    class="relative w-96 mx-auto h-96 border-blue-400 border mt-5"
+  >
+    <div class="AD-item w-10 h-10 bg-purple-200 absolute"></div>
+  </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
+<script>
+import { nextTick, onBeforeUpdate, reactive, ref } from "vue";
+import grab from "./lib/grab";
 
-export default defineComponent({
-  name: "App",
+export default {
   setup() {
-    return {};
+    const list = reactive([1, 2, 3]);
+    const divs = ref([]);
+
+    // Make sure to reset the refs before each update.
+    onBeforeUpdate(() => {
+      divs.value = [];
+    });
+
+    nextTick(() => grab());
+
+    return {
+      list,
+      divs,
+    };
   },
-});
+};
 </script>
