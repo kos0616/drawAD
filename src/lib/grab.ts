281e91727c1ebd709975d344b222f48ff80b41db
Mark <Mark@b.com>
2022-11-25 14:15:28 +0800

    加入畫布範圍設定

--- src/lib/grab.ts
+++ src/lib/grab.ts
@@ -1,5 +1,9 @@
 export default (query = ".AD-item") => {
   const elmnt = document.querySelector(query) as HTMLDivElement;
+  const _CANVAS = document.querySelector("#canvas");
+  if (!elmnt || !_CANVAS) throw new Error("MOVE ITEM ERROR");
+  const CANVAS = _CANVAS.getBoundingClientRect();
+
   /** 滑鼠拖曳後的相對移動x */
   let pos1 = 0;
   /** 滑鼠拖曳後的相對移動y */
@@ -9,7 +13,6 @@ export default (query = ".AD-item") => {
   /** 滑鼠起始位置y */
   let pos4 = 0;
 
-  if (!elmnt) throw new Error("MOVE ITEM ERROR");
   elmnt.onmousedown = dragMouseDown;
 
   function dragMouseDown(e: MouseEvent) {
@@ -26,14 +29,47 @@ export default (query = ".AD-item") => {
   function elementDrag(e: MouseEvent) {
     e = e || window.event;
     e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
+    const rect = elmnt.getBoundingClientRect();
+
     // set the element's new position:
-    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
-    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+    moveX();
+    moveY();
+
+    // calculate the new cursor position:
+    function moveY() {
+      pos2 = pos4 - e.clientY;
+      const top_limit = elmnt.offsetTop - pos2 <= 0;
+      const bottom_limit = rect.height + rect.y >= CANVAS.y + CANVAS.height;
+
+      if (top_limit) {
+        elmnt.style.top = "0px";
+        return;
+      }
+      if (bottom_limit && pos2 < 0) {
+        elmnt.style.top = CANVAS.height - rect.height + "px";
+        return;
+      }
+
+      elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+      pos4 = e.clientY;
+    }
+
+    function moveX() {
+      pos1 = pos3 - e.clientX;
+      const left_limit = elmnt.offsetLeft - pos1 <= 0;
+      const right_limit = rect.width + rect.x >= CANVAS.x + CANVAS.width;
+
+      if (left_limit) {
+        elmnt.style.left = "0px";
+        return;
+      }
+      if (right_limit && pos1 < 0) {
+        elmnt.style.left = CANVAS.width - rect.width + "px";
+        return;
+      }
+      elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+      pos3 = e.clientX;
+    }
   }
 
   function closeDragElement() {
